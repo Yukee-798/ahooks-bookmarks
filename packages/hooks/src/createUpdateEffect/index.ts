09b7916b86f5e45332a90072df7422a700db6807
Yukee-798 <yukee798@gmail.com>
2023-10-22 12:58:55 +0800

    feat: useUpdateEffect

--- packages/hooks/src/createUpdateEffect/index.ts
+++ packages/hooks/src/createUpdateEffect/index.ts
@@ -5,9 +5,10 @@ type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 
 export const createUpdateEffect: (hook: EffectHookType) => EffectHookType =
   (hook) => (effect, deps) => {
+    // 使用 isMountedRef 来判断组件是否已挂载
     const isMounted = useRef(false);
 
-    // for react-refresh
+    // for react-refresh，记得在组件卸载的时候重置 isMounted
     hook(() => {
       return () => {
         isMounted.current = false;
@@ -15,6 +16,7 @@ export const createUpdateEffect: (hook: EffectHookType) => EffectHookType =
     }, []);
 
     hook(() => {
+      // 内部是一个 if else，如果组件已经挂载了才会执行 effect
       if (!isMounted.current) {
         isMounted.current = true;
       } else {
