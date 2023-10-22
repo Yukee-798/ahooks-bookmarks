79a2981e13bc5636ed891d7cb16767b27573212d
Yukee-798 <yukee798@gmail.com>
2023-10-22 12:58:38 +0800

    feat: useMemoizedFn

--- packages/hooks/src/useMemoizedFn/index.ts
+++ packages/hooks/src/useMemoizedFn/index.ts
@@ -10,24 +10,33 @@ type PickFunction<T extends noop> = (
 ) => ReturnType<T>;
 
 function useMemoizedFn<T extends noop>(fn: T) {
-  if (isDev) {
-    if (!isFunction(fn)) {
-      console.error(`useMemoizedFn expected parameter is a function, got ${typeof fn}`);
-    }
-  }
-
   const fnRef = useRef<T>(fn);
 
   // why not write `fnRef.current = fn`?
   // https://github.com/alibaba/hooks/issues/728
+  // 让每一次组件重新渲染时，fnRef.current 都保持为最新的 fn 引用，这么做是为了解决 react devtool 的 bug
   fnRef.current = useMemo(() => fn, [fn]);
+  // 下面的代码等价
+  // fnRef.current = fn;
 
   const memoizedFn = useRef<PickFunction<T>>();
+  // 这里的 memoizedFn.current 为
   if (!memoizedFn.current) {
     memoizedFn.current = function (this, ...args) {
       return fnRef.current.apply(this, args);
     };
   }
+
+  // 思考一下下面两种实现方式有什么不同？
+  // if (!memoizedFn.current) {
+  //   memoizedFn.current = function (this, ...args) {
+  //     return fnRef.current.apply(this, args);
+  //   };
+  // }
+
+  // if (!memoizedFn.current) {
+  //   memoizedFn.current = fnRef.current;
+  // }
 
   return memoizedFn.current as T;
 }
